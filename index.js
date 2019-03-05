02034844f953987b7d703e1160291fa59551a58b
pedromalsavis <petertitoy@protonmail.com>
2019-03-06 01:22:33 +0800

    basic callback for index.js

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
 const http = require('http')
 const url = require('url')
-const {StringDecoder} = require('string_decoder')
+const stringDecoder = require('string_decoder').StringDecoder
 
-console.log({...stringDecoder})
-const server = http.createServer((req, res) => {
-  //parse the url
-  const parseurl = url.parse(req.url, true)
-  const pathname = parseurl.pathname.replace(/^\/+|\/+$/g, '')
-  //get its method
-  const method = req.method.toLowerCase()
-  //get its query
-  const queryString = req.pathname.query
-  //get its headers
-  const headers = req.headers
-  //get payload
-  let buff = ''
-  const pay = new StringDecoder('utf8')
-  res.on('data', (data) => {
-    buff += pay.write(data)
-  })
-  res.on('end', () => {
-    buff += pay.end()
+//local libraries
+const handlers = require('./lib/handlers/main')
 
-    //route handling 
-    const chosenroute = typeof(router[pathname]) !== 'undefind' ? router[pathname] : handlers.pageNotFound
-
-    chosenroute(data, (statusCode, payload)) => {
-
-    }
-
-  })
-})
-
-server.listen(3000, ()=>{
+http.createServer((req, res) => {
+  unisonServer(req, res)
+}).listen(3000, ()=>{
   console.log('server is listening to 3000')
 })
+
+
+
+const unisonServer = (req, res) => {
+  //instance for data, headers, and method handling
+  const parseURL = url.parse(req.url, true)
+  const path = parseURL.pathname
+  const trimpath = path.replace(/^\/+|\/+$/g, '')
+  const queryStringObj = parseURL.query
+  const method = req.method.toLowerCase()
+  const headers = req.headers
+  const decoder = new stringDecoder('utf-8')
+
+  //instance for handling body-data
+  let buffer = ''
+
+  //request for data
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+
+  //request for end
+  req.on('end', () => {
+    buffer += decoder.end()
+    const chosenHandler = typeof(router[trimpath]) !== 'undefined' ? router[trimpath] : handlers.pageNotFound
+    const data = {
+      trimpath,
+      method,
+      queryStringObj,
+      headers,
+      'payload': buffer
+    }
+
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+      payload = typeof(payload) == 'object' ? payload : {}
+      const payloadString = JSON.stringify(payload)
+      res.setHeader('Content-type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+      console.log('return this response', statusCode, payloadString)
+    })
+  })
+}
+
+const router = {
+  'sample' : handlers.sample
+}
